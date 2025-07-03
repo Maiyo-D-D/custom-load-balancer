da68be87e16e527b388e3337ab9e78e89749df60
Leon Bundi <leon.bundi@strathmore.edu>
2025-07-03 15:56:40 +0300

    readme update-docker setup

---
+++ server.py
@@ -0,0 +1,19 @@
+# server.py
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+# Get server ID from environment variable
+SERVER_ID = os.getenv("SERVER_ID", "unknown")
+
+@app.route("/home", methods=["GET"])
+def home():
+    return {"message": f"Hello from Server: {SERVER_ID}", "status": "successful"}, 200
+
+@app.route("/heartbeat", methods=["GET"])
+def heartbeat():
+    return {}, 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
